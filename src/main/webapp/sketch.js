6dc4a1d83fc1caa3bbe9ff7e3c12a1499ed996c4
Amit-Roy <a.roy.0593@gmail.com>
2017-08-16 13:45:00 +0530

    more fun
    Signed-off-by: Amit-Roy <a.roy.0593@gmail.com>

--- src/main/webapp/sketch.js
+++ src/main/webapp/sketch.js
@@ -2,19 +2,35 @@ var counter = 0;
 
 
 function setup() {
-    fill(0);
     createCanvas(windowWidth, windowHeight);
+    background(0);
 }
 
 function draw() {
     counter++;
-    if (counter % 2 === 0) {
-        fill(255, 0, 0);
-    } else if (counter % 3 === 0) {
-        fill(0, 0, 255);
-    }
-    else {
-        fill(0, 255, 0);
+    counter = counter % 7;
+    switch (counter) {
+        case 0:
+            fill(255, 0, 0);
+            break;
+        case 1:
+            fill(255, 127, 0);
+            break;
+        case 2:
+            fill(255, 255, 0);
+            break;
+        case 3:
+            fill(0, 255, 0);
+            break;
+        case 4:
+            fill(0, 0, 255);
+            break;
+        case 5:
+            fill(75, 0, 130);
+            break;
+        case 6:
+            fill(139, 0, 255);
+            break;
     }
 
     ellipse(mouseX, mouseY, 60, 60);
